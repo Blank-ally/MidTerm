56d4f601093869d1e7d8010eae9882293698703c
Blank-ally <lia.stith@gmail.com>
2023-02-28 12:53:18 -0600

    Add project files.

---
+++ RaceTrack/Cars/ElectricCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+
+    internal class ElectricCar : RaceCar
+    {
+
+        public ElectricCar()
+        {
+            Name = "Electric Car";
+            TopSpeed = 100;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} buzzes to life");
+        }
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} grinds to a hault");
+            base.Brake();
+        }
+    }
+}
---
+++ RaceTrack/Drivers/Margret.cs
@@ -0,0 +1,23 @@
+﻿using RaceTrack.RaceTrack.Cars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    internal class Margret : Driver
+    {
+        public Margret(RaceCar car) : base(car)
+        {
+            Name = "Margret";
+            SkillLevel = 15;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
